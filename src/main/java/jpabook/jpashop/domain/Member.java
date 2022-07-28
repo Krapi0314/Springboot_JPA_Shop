95037b223366812f7f4e2d30999fce33bd9f974e
Komputer-P <wlghks0314@gmail.com>
2022-07-28 18:56:56 +0900

    [Study] 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 섹션 1,2,3,4,5장

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // mappedBy: 연관관계에서 참조만 하는 곳 (거울 같은)이라고 명시
+    private List<Order> orders = new ArrayList<>();
+}
